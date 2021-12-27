3784e1e8cb9206861968d8fb1a89f5bbb52233f5
EkoEko <lauff1234@gmail.com>
2021-12-27 11:54:10 +0100

    try

--- dist/controllers/users/briefUser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const briefUser = async (req, res) => {
-    const { photo, username, hobby, email } = req.user;
-    res.status(200).send({ photo, username, hobby, email });
-};
-exports.default = briefUser;
-//# sourceMappingURL=briefUser.js.map
