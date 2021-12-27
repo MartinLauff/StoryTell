440984c1597d4ed4e6657d2dd1849475ac831aca
EkoEko <lauff1234@gmail.com>
2021-12-27 23:14:06 +0100

    try

--- dist/controllers/auth/signout.js
+++ dist/controllers/auth/signout.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const signout = (_req, res) => {
+const signout = (req, res) => {
     res.cookie('jwt', 'loggedout', {
         expires: new Date(Date.now() + 10 * 1000),
-        httpOnly: true,
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
     });
     res.status(200).send();
 };
