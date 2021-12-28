2bfcfbfddc4223c56fad22071ef71776e3938d3d
EkoEko <lauff1234@gmail.com>
2021-12-28 18:24:00 +0100

    try

--- pages/500.js
+++ pages/500.js
@@ -4,23 +4,27 @@ import SingleButton from '../components/SingleButton';
 
 export default function Custom500() {
   return (
-    <div className={errorStyles.custom500}>
-      <h1>You need to be logged in to get access</h1>
-      <Link href='/auth/signin'>
-        <a>
-          <SingleButton
-            style={{ margin: '3.5rem 0 2.5rem' }}
-            route='/signin'
-            content='Sign in'
-            color='whiteButton'
-          />
-        </a>
-      </Link>
-      <Link href='/auth/signup'>
-        <a>
-          <SingleButton route='/signup' content='Sign up' color='redButton' />
-        </a>
-      </Link>
+    <div className={errorStyles.errWrap}>
+      <div className={errorStyles.custom500}>
+        <h1 className={errorStyles.h500}>
+          You need to be logged in to get access
+        </h1>
+        <Link href='/auth/signin'>
+          <a>
+            <SingleButton
+              style={{ margin: '3.5rem 0 2.5rem' }}
+              route='/signin'
+              content='Sign in'
+              color='whiteButton'
+            />
+          </a>
+        </Link>
+        <Link href='/auth/signup'>
+          <a>
+            <SingleButton route='/signup' content='Sign up' color='redButton' />
+          </a>
+        </Link>
+      </div>
     </div>
   );
 }
